d6d26d24ee6c56aec112da7f4ce8afacf83eff48
Faisal963 <faisal.alhag@student.hu.nl>
2021-04-19 17:29:56 +0200

    last commite
    Card list component toegevoegd
    Card component toegevoegd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor () {
+    super();
+    /* this.state = {
+      string:'Hello'
+    }; */
+    this.state = {
+      developers: [
+        {
+          name: 'Max',
+          id:'dev1'
+        },
+        {
+          name: 'Jonty',
+          id:'dev2'
+        },
+        {
+          name: 'Nabil',
+          id:'dev3'
+        },
+        {
+          name: 'Darius',
+          id:'dev4'
+        }
+      ],
+      searchField:""
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({developers: users }));
+  }
+
+  render() {
+    const { developers, searchField } = this.state;
+    const filteredDevelopers= developers.filter(
+      developer => developer.name.toLowerCase().includes(
+        searchField.toLowerCase()))
+    return (
+      <div className="App">
+        <input 
+          type="search" 
+          placeholder="search developers" 
+          onChange={e => this.setState({ searchField: e.target.value })}/>
+
+        <CardList developers={filteredDevelopers}>
+          
+        </CardList>
+      </div>
+    );
+  }
 }
 
 export default App;
+/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{this.state.string}</p>
+          <button onClick={() => this.setState({string: 'Hello Max'})}>
+            Change Text
+          </button>
+        </header> */
